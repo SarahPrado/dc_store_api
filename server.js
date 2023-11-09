fa71431c47b656435a9f5e95561108e153698e8f
Sarah Prado Medeiros <SARAH0253@DIGITALCOLLEGE.LOCAL>
2023-11-09 20:06:01 -0300

    Inicializando o express

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+//express serve para configurar o http
+
+const app = express ()
+
+//app.use é configurando o express
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true}))
+
+app.get('/', (req, res) => {
+    res.json({message: 'Servidor rodando, eee'})
+    // res.json('Servidor rodando!')
+})
+
+const HOST = 'localhost'
+const PORT = '5000'
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em: ${HOST}: ${PORT}`);
+})
